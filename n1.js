ba4a966ec42ebfe61021791a79baf9cac11480fc
Ganesh Kumar <vikkyganesh47@gmail.com>
2019-04-14 00:48:07 +0530

    Bran Commit

--- n1.js
+++ n1.js
@@ -1,11 +1,1 @@
 const MongoClient = require('mongodb').MongoClient
-const uri = process.env.MONGODB_URI || `mongodb://localhost:27017`
-const dbName ='tan'
-const collectionName = 'tick'
-let client = MongoClient.connect(uri, { useNewUrlParser: true })
-let dbObject = client
-  .then(clientObj => {
-    console.log('Connection to DB successful')
-    return clientObj.db(dbName).collection(collectionName)
-  })
-
